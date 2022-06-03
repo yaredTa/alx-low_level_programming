ed4ecc110fa3b437a98ac4687a0c1b582c45264c
yaredTa <yareda6@gmail.com>
2022-06-04 01:40:06 +0300

    Love

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main- to print "Programming is like building a multilingual puzzle
+ *
+ * Return: 0 if exited properly, non-zero otherwise
+*/
+int main() {
+char c;
+
+for (c = 'a'; c <= 'z'; ++c)
+    putchar(c);
+return 0;
+}
