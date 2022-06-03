8994973a7003ca183d8bee8769db356a62100883
yaredTa <yareda6@gmail.com>
2022-06-04 00:39:43 +0300

    Love

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,9 +4,9 @@
 
 /**
  *main- to print "Programming is like building a multilingual puzzle
-* 
-*Return: 0 if exited properly, non-zero otherwise 
- */
+ *
+ * Return: 0 if exited properly, non-zero otherwise
+*/
 
 int main(void)
 {
