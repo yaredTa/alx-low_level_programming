ff361d6bef2f07799e1a11ff53258118e553ed87
yaredTa <yareda6@gmail.com>
2022-06-03 16:41:30 +0300

    Love

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,7 @@
 
 /**
  * main- to print "Programming is like building a multilingual puzzle
- * returns 0
- */
-*Return: 0 if exited properly, non-zero otherwise
+ * Return: 0 if exited properly, non-zero otherwise
 */
 int main(void)
 {
