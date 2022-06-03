4343d81ab47ed854303a387210a969d4273a0fff
yaredTa <yareda6@gmail.com>
2022-06-04 02:34:53 +0300

    Love

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main- to print "Programming is like building a multilingual puzzle
+ *
+ * Return: 0 if exited properly, non-zero otherwise
+*/
+int main(void)
+{
+char c;
+
+for (c = 'z'; c >= 'a'; c--)
+putchar(c);
+
+putchar('\n');
+return (0);
+}
