660f0ca0dd4fd606bf21eb88220659f3edbda306
yaredTa <yareda6@gmail.com>
2022-06-04 02:48:22 +0300

    Love

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main- to print "Programming is like building a multilingual puzzle
+ *
+ * Return: 0 if exited properly, non-zero otherwise
+*/
+int main(void)
+{
+int c;
+
+for (c = 0; c < 10; c++)
+{
+putchar(c + '0');
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
