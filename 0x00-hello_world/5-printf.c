133ce371882fb1c6e5358cf82fd555a2d9542e61
yaredTa <yareda6@gmail.com>
2022-06-03 16:44:18 +0300

    Love

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main- to print "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
