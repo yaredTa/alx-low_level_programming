4b540fd06f385bbe0e32d6e03700791cdddd6a79
yaredTa <yareda6@gmail.com>
2022-06-03 16:34:40 +0300

    Love

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+/*main function to print the sentence "Programing is like building a multilingual puzzle
+*
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
