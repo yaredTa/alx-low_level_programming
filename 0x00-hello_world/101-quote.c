c8cc5e7b25f1f7420a40c5617645247fa6674f6a
yaredTa <yareda6@gmail.com>
2022-06-03 20:14:44 +0300

    Love

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - prints the string and that piece of art is useful" - Dora Korpar, 2015-10-19
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}
