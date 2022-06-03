b5d256b581977598a520688ca1947630d6e44e85
yaredTa <yareda6@gmail.com>
2022-06-04 02:13:37 +0300

    Love

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main- to print "Programming is like building a multilingual puzzle
+ *
+ * Return: 0 if exited properly, non-zero otherwise
+*/
+int main(void)
+{
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+
+for (c = 'A'; c <= 'Z'; c++)
+putchar(c);
+
+putchar('\n');
+return (0);
+}
