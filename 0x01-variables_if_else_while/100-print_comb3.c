6f61f3a5d1b6fa7b61b5a6f594828e907cdb9ec0
yaredTa <yareda6@gmail.com>
2022-06-04 03:17:36 +0300

    Love

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *main- to print "Programming is like building a multilingual puzzle
+ *
+ * Return: 0 if exited properly, non-zero otherwise
+*/
+int main(void)
+{
+int c;
+int d;
+
+for (c = 0; c < 9; c++)
+{
+for(d = c+1 ;d < 10 ; d++)
+{
+if (c == 8 && d == 9)
+printf("%d%d", c, d);
+else
+printf("%d%d, ", c, d);
+}
+}
+
+printf("\n");
+return (0);
+}
