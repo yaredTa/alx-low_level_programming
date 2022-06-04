42e9de7eaeca69e1faf4561cd3dfbb1fc925db3a
yaredTa <yareda6@gmail.com>
2022-06-04 03:27:12 +0300

    Love

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *main- to print "Programming is like building a multilingual puzzle
+ *
+ * Return: 0 if exited properly, non-zero otherwise
+*/
+int main(void)
+{
+int c;
+int d;
+int e;
+
+for (c = 0; c < 8; c++)
+{
+for (d = c + 1 ; d < 9 ; d++)
+{
+for (e = d + 1 ; e < 10 ; e++)
+{
+if (c == 7 && d == 8 && e == 9)
+putchar(c + '0');
+putchar(d + '0');
+putchar(e + '0');
+
+else
+putchar(c + '0');
+putchar(d + '0');
+putchar(e + '0');
+putchar(',');
+putchar(' ');
+}
+}
+}
+
+printf("\n");
+return (0);
+}
