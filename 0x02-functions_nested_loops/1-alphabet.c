700a9eddb73be1c88e6ef7c5169c4352b85a5b90
yaredTa <yareda6@gmail.com>
2022-06-08 02:30:52 +0300

    LOVE

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ *main- to print "Programming is like building a multilingual puzzle
+ *
+ * Return: 0 if exited properly, non-zero otherwise
+*/
+int void (void)
+{
+char c = 'a';
+
+while (c <= 'z')
+{
+_putchar(c);
+}
+_putchar('\n');
+
+return (0);
+
+}
