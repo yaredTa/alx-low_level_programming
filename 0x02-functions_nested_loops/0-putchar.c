70a7fedddb72cbae88f1806fbb4acc29f06bbad5
yaredTa <yareda6@gmail.com>
2022-06-08 01:43:20 +0300

    LOVE

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main- to print "Programming is like building a multilingual puzzle
+ *
+ * Return: 0 if exited properly, non-zero otherwise
+*/
+
+int main()
+{  
+char b[] ="_putchar";
+for(int i = 0; i < sizeof b / sizeof *b; i++ )
+    putchar(b[i]);
+
+return (0);
+}
