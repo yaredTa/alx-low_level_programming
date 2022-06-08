30e2ebff3a7d8bb4bbeada465b3d48c7f0aa5e2d
yaredTa <yareda6@gmail.com>
2022-06-08 04:14:54 +0300

    LOVE

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,6 +4,7 @@
 
 /**
  * print_sign - print the sign of the number
+ * @n: the int to check
  * Returns 1 and prints + if n is greater than zero
  * Returns 0 and prints 0 if n is zero
  * Returns -1 and prints - if n is less than zero
