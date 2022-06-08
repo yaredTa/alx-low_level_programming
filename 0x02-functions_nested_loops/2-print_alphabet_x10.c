2574bb7039c6ed4478117ff75d6d5b6d79d9ca99
yaredTa <yareda6@gmail.com>
2022-06-08 03:14:43 +0300

    LOVE

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,7 +3,7 @@
 #include "main.h"
 
 /**
- * print print_alphabet_x10 is to print the alpahbet in lower case 10 times
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
  * followed by a new line
  *
 */
