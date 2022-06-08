15ca27642e705f6fb7fe257e18aa7378cd22324a
yaredTa <yareda6@gmail.com>
2022-06-08 05:43:39 +0300

    LOVE

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,9 +4,9 @@
 
 /**
  * add - adds two integers and returns the result
- * @a: int to be added to b
- * @b: int to be added to a
- * Return: sum of a and b
+ * @a: int to be added to n
+ * @b: int to be added to m
+ * Return: sum of n and m
  */
 int add(int n, int m)
 {
