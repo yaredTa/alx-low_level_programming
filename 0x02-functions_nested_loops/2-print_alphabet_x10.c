daa3ac9f069aab280af00b9764c062b674c63ff8
yaredTa <yareda6@gmail.com>
2022-06-08 03:10:03 +0300

    LOVE

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,12 +3,11 @@
 #include "main.h"
 
 /**
- *print print_alphabet_x10 is to print the alpahbet 10 times
+ *print print_alphabet_x10 is to print the alpahbet in lower case 10 times
  *
 */
 void print_alphabet_x10(void)
 {
-
 int b = 1;
 
 while (b <= 10)
