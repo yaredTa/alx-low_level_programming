26850a92f3a20914145a9209627d7a59e664a38a
yaredTa <yareda6@gmail.com>
2022-06-08 05:13:29 +0300

    LOVE

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,17 +8,21 @@
 void jack_bauer(void)
 {
 int m = 0;
-int n = 0;
-int p = 0;
-int q = 0;  
+
 while (m <= 2)
 {
+int n = 0;
+  
 while ((n <= 9 && m <= 1) || (n <= 3 && m <= 2))
 {
+int p = 0;
+
 while (p <= 5)
 {
+int q = 0;
 while (q <= 9)
 {
+
 _putchar(m + '0');
 _putchar(n + '0');
 _putchar(':');
