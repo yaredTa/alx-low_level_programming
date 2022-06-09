2f7f4a37fb074af604e4bf715f5c63b70d892cab
yaredTa <yareda6@gmail.com>
2022-06-09 20:42:58 +0300

    LOVE

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -14,3 +14,19 @@ positive_or_negative(i);
 
 return (0);
 }
+int positive_or_negative(int n)
+{
+	
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+if (n > 0)
+{
+printf("%d is positive\n", n);
+}
+else if (n == 0)
+printf("%d is zero\n", n);
+else
+printf("%d is negative\n", n);
+return (0);
+}
