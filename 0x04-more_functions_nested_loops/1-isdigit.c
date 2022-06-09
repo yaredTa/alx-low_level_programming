3599c2616c581537934b39c16b993abd4836f448
yaredTa <yareda6@gmail.com>
2022-06-09 22:24:06 +0300

    LOVE

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -4,9 +4,9 @@
 
 
 /**
- * _isdigit -  checks for digits
+ * _isdigit - checks for a digit (0 through 9)
  * @c: int to be checked
- * Return: 1 if c is digit, 0 otherwise
+ * Return: 1 if c is a digit, 0 otherwise
  */
 int _isdigit(int c)
 {
