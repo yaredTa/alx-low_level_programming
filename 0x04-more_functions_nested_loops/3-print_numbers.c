5b0f6bb2f464f6c5632314dd928bd54d6ccaf473
yaredTa <yareda6@gmail.com>
2022-06-09 23:15:10 +0300

    LOVE

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -8,13 +8,13 @@
  */
 void print_numbers(void)
 {
-	int i = 0;
+int i = 0;
 
-	while (i < 10)
-	{
-		_putchar(i + '0');
+while (i < 10)
+{
+_putchar(i + '0');
 i++;
-	}
+}
 
-	_putchar('\n');
+_putchar('\n');
 }
