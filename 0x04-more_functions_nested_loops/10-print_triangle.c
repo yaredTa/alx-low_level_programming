87a80eff514f5403733dc531088c5db79a34e734
yaredTa <yareda6@gmail.com>
2022-06-10 06:07:35 +0300

    LOVE

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -15,14 +15,14 @@ void print_triangle(int size)
 
 		for (i = 1; i <= size; i++)
 		{
-			for (j = 1; j <= i; j++)
-			{
-				_putchar(' ');
-			}
-
 			for (j = i + 1; j <= size; j++)
 			{
 				_putchar('#');
+			}
+
+			for (j = 1; j <= i; j++)
+			{
+				_putchar(' ');
 			}
 
 			_putchar('\n');
