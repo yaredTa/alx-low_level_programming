dbd7bae674555ab4a2863d6a34e8347d0b0e7fdb
yaredTa <yareda6@gmail.com>
2022-06-10 14:20:50 +0300

    LOVE

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -7,22 +7,10 @@
 
 int main(void)
 {
-int i;
+        int i;
 
-i = 98;
-positive_or_negative(i);
+        i = 0;
+        positive_or_negative(i);
 
-return (0);
-}
-int positive_or_negative(int n)
-{
-if (n > 0)
-{
-printf("%d is positive\n", n);
-}
-else if (n == 0)
-printf("%d is zero\n", n);
-else
-printf("%d is negative\n", n);
-return (0);
+        return (0);
 }
