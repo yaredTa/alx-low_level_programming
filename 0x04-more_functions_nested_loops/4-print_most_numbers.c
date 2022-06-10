23b733676bb7ca5484385a6c5de2fd53486339a4
yaredTa <yareda6@gmail.com>
2022-06-10 05:17:27 +0300

    LOVE

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,14 +6,15 @@
  */
 void print_most_numbers(void)
 {
-int i;
-while (i < 10)
-{
-if (i != 2 && i != 4)
-{
-_putchar(i + '0');
-}
-i++;
-}
-_putchar('\n');
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar(i + '0');
+		}
+	}
+
+	_putchar('\n');
 }
