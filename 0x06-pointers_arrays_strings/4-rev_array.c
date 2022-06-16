6c3ad110d7aeea934e557572de0bb1f975420b87
yaredTa <yareda6@gmail.com>
2022-06-16 05:20:23 +0300

    LOVE

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,12 +9,12 @@ void reverse_array(int *a, int n)
 {
 	int i, tmp;
 
-	
+	j = n - 1;
 
 	for (i = 0; i < n / 2; i++)
 	{
 		tmp = a[i];
-		a[i] = a[j];
+		a[i] = a[n - (i+1)];
 		a[n - (i + 1)] = tmp;
 	}
 }
